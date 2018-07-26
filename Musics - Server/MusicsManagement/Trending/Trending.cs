0888d8e9a8bd92254aec4ed66d06fb13a9c6a57f
Malaury <malaurydutour@outlook.fr>
2018-07-26 21:20:56 +0200

    Add a function to get the most liked musics on the server (for trending)

--- Musics - Server/MusicsManagement/Trending/Trending.cs
+++ Musics - Server/MusicsManagement/Trending/Trending.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Utility;
+using Musics___Server.MusicsManagement;
+
 
 namespace Musics___Server.MusicsManagement.Trending
 {
@@ -11,7 +13,17 @@ namespace Musics___Server.MusicsManagement.Trending
     {
         static List<Music> GetMostLikedMusic()
         {
+            Dictionary<Music, int> musics = new Dictionary<Music, int>();
 
+
+            foreach(var m in Indexation.GetAllMusics())
+            {
+                musics.Add(m, m.Rating);
+            }
+
+            musics.ToList().Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+
+            return (from val in musics.Take(10) select val.Key).ToList();
         }
         static List<Music> GetMostLikedMusicByDay()
         {
