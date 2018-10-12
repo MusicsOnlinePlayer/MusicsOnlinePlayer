2244417c19c1779bb496c1fe5c5bdf860bc683b7
Malaury <malaurydutour@outlook.fr>
2018-10-12 20:10:43 +0200

    This add a log file

--- Musics - Server/Commands/Info.cs
+++ Musics - Server/Commands/Info.cs
@@ -1,16 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Musics___Server.Commands
 {
     static class Info
     {
-        public static void Say(string message,MsgType type)
+        public static string LogPath;
+
+        public static void CreateLog()
         {
-            Console.WriteLine("[{0} - {1}] " + message, type.ToString().ToUpper(), DateTime.Now.ToString("HH:mm:ss"));
+            LogPath = "Log"+ DateTime.Now.ToString("HHmmss")+".txt";
+            File.Create(LogPath).Dispose();
+        }
+
+        public static void Say(string message,MsgType type,bool LogInFile = true)
+        {
+            string LogLine = "[" + type.ToString().ToUpper() + " - " + DateTime.Now.ToString("HH:mm:ss") + "] " + message;
+            Console.WriteLine(LogLine);
+            if (LogInFile)
+            {
+                SaveLogLine(LogLine);
+            }
+        }
+
+        public static void SaveLogLine(string LogLine)
+        {
+            if (!File.Exists(LogPath))
+            {
+                CreateLog();
+            }
+            File.AppendAllText(LogPath, LogLine + Environment.NewLine);
         }
     }
 
