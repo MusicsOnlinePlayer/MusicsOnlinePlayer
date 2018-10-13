274f497a706007ea7b7193ffc7e49580b58d2482
Malaury <malaurydutour@outlook.fr>
2018-10-13 10:30:07 +0200

    This implement CodeCraft.Logger

--- Musics - Server/Program.cs
+++ Musics - Server/Program.cs
@@ -14,13 +14,15 @@ using Utility.Network.Dialog.Rating;
 using Utility.Network.Dialog.Edits;
 using Utility.Network.Dialog.Uploads;
 using Utility.Network.Dialog.Authentification;
+using CodeCraft.Logger;
+
 
 namespace Musics___Server
 {
     class Program
     {
         public static Server MyServer = new Server();
-        public static ServerComHandler ServerCom = new ServerComHandler();
+        public static ServerComHandler ServerCom = new ServerComHandler();   
 
         static void Main(string[] args)
         {
@@ -29,10 +31,9 @@ namespace Musics___Server
             MyServer.AuthService.SetupAuth();
             MusicsInfo.SetupMusics();
 
-            Info.Say("Indexation of all musics....  ", MsgType.Info);
-            Info.Say(Indexation.Do(Properties.Settings.Default.UseMultiThreading) + "Musics", MsgType.Info);
-            Info.Say("Indexation done.", MsgType.Info);
-
+            MyServer.Log.Info("Indexation of all musics....  ");
+            MyServer.Log.Info(Indexation.Do(Properties.Settings.Default.UseMultiThreading) + "Musics");
+            MyServer.Log.Info("Indexation done.");
             Indexation.SaveAllInfos();
 
             //Manager.RefreshTrending();
@@ -44,9 +45,9 @@ namespace Musics___Server
                 entry = Console.ReadLine();
                 Commands.Commands.Do(entry);
             }
-            Info.Say("Saving music info ... ", MsgType.Info);
+            MyServer.Log.Info("Saving music info ... ");
             Indexation.SaveAllInfos();
-            Info.Say("Done.", MsgType.Info);
+            MyServer.Log.Info("Done.");
         }
 
         public static void PromoteUser(string UID, Rank rank)
@@ -90,8 +91,7 @@ namespace Musics___Server
             if (ClientLogin)
             {
                 if (received is Request)
-                {
-                    
+                {     
                     Network.Handle.Requests.Handle(received as Request,socket);
                 }
                 if (received is Rate)
@@ -100,7 +100,7 @@ namespace Musics___Server
                 }
                 if(received is Disconnect)
                 {
-                    Info.Say("Client disconnected =(", MsgType.Info);
+                    MyServer.Log.Warn("Client disconnected =(");
                     MyServer.Clients.List.Remove(socket);
                 }
                 if (received is EditUser)
@@ -112,10 +112,12 @@ namespace Musics___Server
 
                         MyServer.Clients.List.Remove(socket);
                         MyServer.Clients.AddUser(tmp.NewUser, socket);
+                        MyServer.Log.Warn("User " + tmp.NewUser + "has been edited");
                         return;
                     }
                     else
                     {
+                        MyServer.Log.Warn("Editing the user " + tmp.NewUser + " failed !");
                         MyServer.SendObject(new EditUserReport(false, tmp.NewUser), socket);
                     }
                 }
@@ -134,13 +136,22 @@ namespace Musics___Server
                                     UsersInfos.GetUser(tmp.UserToEdit)
                                 };
                                 MyServer.SendObject(new RequestAnswer(tmpU, true), socket);
-                                Info.Say("User promoted " + tmp.UserToEdit + " to " + tmp.NewRankOfUser.ToString(),MsgType.Important);
+                                MyServer.Log.Warn("User promoted " + tmp.UserToEdit + " to " + tmp.NewRankOfUser.ToString());
+                            }
+                            else
+                            {
+                                MyServer.Log.Warn("Promoting the user " + tmp.UserToEdit + " to " + tmp.NewRankOfUser.ToString()+" failed !");
                             }
                             break;
                         case TypesEdit.Musics:
                             if ((int)MyServer.Clients.GetUser(socket).Userrank > 1)
                             {
                                 Indexation.ModifyElement(tmp.ObjectToEdit as Element, tmp.NewName ,tmp.NewGenres);
+                                MyServer.Log.Warn("The musics " + tmp.NewName + " has been edited !");
+                            }
+                            else
+                            {
+                                MyServer.Log.Warn("The musics " + tmp.NewName + " couldn't be edited");
                             }
                             break;
                     }
@@ -149,6 +160,7 @@ namespace Musics___Server
                 {
                     SavePlaylist tmp = received as SavePlaylist;
                     UsersInfos.SaveUserPlaylist(tmp.UID, tmp.Playlist);
+                    MyServer.Log.Info("The playlist " + tmp.Playlist.Name + "has been created");
                 }
                 if(received is UploadMusic)
                 {
@@ -156,10 +168,13 @@ namespace Musics___Server
                     if (Indexation.AddElement(tmp) && (int)MyServer.Clients.GetUser(socket).Userrank > 1)
                     {
                         MyServer.SendObject(new UploadReport(null, true),socket);
+                        MyServer.Log.Warn("The music "+ tmp.MusicPart.Name +"has been upload");
                     }
                     else
                     {
                         MyServer.SendObject(new UploadReport(null, false), socket);
+                        MyServer.Log.Warn("The music " + tmp.MusicPart.Name + "has been upload");
+                        MyServer.Log.Warn("Upload completed with success");
                     }
                 }
             }
@@ -169,7 +184,7 @@ namespace Musics___Server
                 {
                     Login auth = received as Login;
 
-                    Info.Say("Client try to login", MsgType.Info);
+                    MyServer.Log.Warn("Client try to login");
 
                     if (auth.IsSignup)
                     {
