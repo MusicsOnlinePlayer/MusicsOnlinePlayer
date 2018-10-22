15e75d1160b5e1629ff632c85da3807dd410bb30
Malaury <malaurydutour@outlook.fr>
2018-10-22 19:51:27 +0200

    Simplify of the NetworkClient

--- ControlLibrary/Network/Client.cs
+++ ControlLibrary/Network/Client.cs
@@ -9,9 +9,9 @@ namespace ControlLibrary.Network
 {
     public static class NetworkClient
     {
-        public static Socket _clientSocket { get; set; }
+        
         public static IPAddress ip = IPAddress.Loopback;
-
+            
         public static Thread recevoir;
 
         private static readonly int Bufferlgth = 100000000;
@@ -26,37 +26,11 @@ namespace ControlLibrary.Network
         {
             if (IPAddress.TryParse(AppSettings.ApplicationSettings.Get().ServerIp, out IPAddress iPAddress))
             {
-                IPEndPoint ip = new IPEndPoint(iPAddress, 2003);
-                try
-                {
-                    _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
-                    {
-                        SendTimeout = 600000,
-                        ReceiveTimeout = 600000
-                    };
-                    _clientSocket.BeginConnect(ip, new AsyncCallback(ConnectCallBack), null);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
+                Network.Connect(iPAddress);
             }
         }
 
-        private static void ConnectCallBack(IAsyncResult ar)
-        {
-            try
-            {
-                _clientSocket.EndConnect(ar);
-
-                recevoir = new Thread(new ThreadStart(Receive));
-                recevoir.Start();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
+        
 
         public static void Receive()
         {
