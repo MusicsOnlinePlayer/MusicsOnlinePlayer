5abff7ac32f8bd1ea23510293ef2360a0fa2662f
Malaury <malaurydutour@outlook.fr>
2018-10-28 18:06:15 +0100

    Adding a common class for all services

---
+++ Musics - Client/API/Services.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musics___Client.API
+{
+    public class Services
+    {
+        public SearchServices Search;
+        public EditMusicsServices EditMusics;
+        public RateServices RateServices;
+        public PlaylistServices PlaylistServices;
+
+        public void Init()
+        {
+            Search = new SearchServices();
+            EditMusics = new EditMusicsServices();
+            RateServices = new RateServices();
+            PlaylistServices = new PlaylistServices();
+        }
+    }
+}
