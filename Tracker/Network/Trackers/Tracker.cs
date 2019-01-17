d2054a4de1baedcd414aecb787215dff722d0018
Malaury <malaurydutour@outlook.fr>
2019-01-17 19:48:17 +0100

    Adding xml save file

--- Tracker/Network/Trackers/Tracker.cs
+++ Tracker/Network/Trackers/Tracker.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Tracker.ServersXML;
 using Utility.Network.Server;
 using Utility.Network.Tracker.Identity;
 using Utility.Network.Tracker.Requests;
@@ -49,6 +50,21 @@ namespace Tracker.Network.Trackers
             Logger.Info("Event Setup...");
             EventSetup();
             Logger.Info("Done.");
+            Logger.Info("Setup tracker xml");
+            ServerXml.Setup();
+            Logger.Info("Done.");
+            AddSeverIdXML();
+        }
+
+        private void AddSeverIdXML()
+        {
+            Console.Write("Retreiving server datas from xml ");
+            foreach(var si in ServerXml.GetServers())
+            {
+                Idlist.Add(null, si);
+                Console.Write(".");
+            }
+            Console.WriteLine("Ok");
         }
 
         private void EventSetup()
@@ -88,11 +104,13 @@ namespace Tracker.Network.Trackers
 
         public void UpdateServersAvailabilty()
         {
-            foreach(var p in Idlist.GetServerID())
+            Logger.Info($"Updating Server Availability...");
+            foreach (var p in Idlist.GetServerID())
             {
                 var pc = p;
                 pc.IsAvailable = CheckServer(pc.IPEndPoint);
-            }    
+            }
+            Logger.Info($"Done");
         }
 
         public bool CheckServer(IPEndPoint ip)
@@ -107,6 +125,21 @@ namespace Tracker.Network.Trackers
             {
                 return false;
             }
+        }
+
+        public bool AddServer(string field)
+        {
+            string[] ip = field.Split(':');
+
+            if(!IPAddress.TryParse(ip[0], out IPAddress iPAddress))
+                return false;
+            if (!int.TryParse(ip[1], out int port))
+                return false;
+
+            var s = new ServerIdentity() { IPEndPoint = new IPEndPoint(iPAddress,port)};
+            Idlist.AddIdentity(null, s); //TODO See Null
+            ServerXml.AddServerToXml(s);
+            return true;
         }
     }
 }
---
+++ Tracker/ServersXML/ServersXml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Linq;
+using System.Net;
+using Utility.Network.Tracker.Identity;
+
+namespace Tracker.ServersXML
+{
+    public static class ServerXml
+    {
+        public const string Path = @"ServersIP.xml";
+
+        public static void Setup()
+        {
+            if (!File.Exists(Path))
+            {
+                using (var writer = XmlWriter.Create(Path))
+                {
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("Servers");
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                }
+            }
+        }
+
+        public static void AddServerToXml(ServerIdentity si)
+        {
+            if (si == null) return;
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Path);
+            
+            XmlNode nodeServer = doc.CreateElement("Server");
+
+            XmlNode nodeIP = doc.CreateElement("Ip");
+            nodeIP.InnerText = si.IPEndPoint.Address.ToString();
+            nodeServer.AppendChild(nodeIP);
+
+            XmlNode nodePort = doc.CreateElement("Port");
+            nodePort.InnerText = si.IPEndPoint.Port.ToString();
+            nodeServer.AppendChild(nodePort);
+
+            doc.DocumentElement.AppendChild(nodeServer);
+            
+            doc.Save(Path);
+
+        }
+
+        public static ServerIdentity[] GetServers()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Path);
+            XmlNodeList nodes = doc.DocumentElement.SelectNodes("Server");
+            return nodes.Cast<XmlNode>().Select(x => GetServer(x)).ToArray();
+
+        }
+
+        public static ServerIdentity GetServer(XmlNode node)
+        {
+            if (!IPAddress.TryParse(node["Ip"].InnerText, out IPAddress iP)) throw new Exception();
+            if (!int.TryParse(node["Port"].InnerText, out int i)) throw new Exception();
+            var si = new ServerIdentity()
+            {
+                IPEndPoint = new IPEndPoint(iP, i)
+            };
+
+            return si;
+        }
+    }
+}
