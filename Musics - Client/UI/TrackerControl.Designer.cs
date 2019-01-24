0dd8d883198e979594a1ca6881dd6b62e9bd4a2a
Malaury <malaurydutour@outlook.fr>
2019-01-24 19:18:31 +0100

    Adding tracker UI

---
+++ Musics - Client/UI/TrackerControl.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Musics___Client.UI
+{
+    partial class TrackerControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label12 = new System.Windows.Forms.Label();
+            this.UIStatut = new System.Windows.Forms.Label();
+            this.UITrackers = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UITextboxSearch = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.UIUpload = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Raleway Medium", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label12.Location = new System.Drawing.Point(8, 7);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(340, 37);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "Trackers connection";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UIStatut
+            // 
+            this.UIStatut.AutoSize = true;
+            this.UIStatut.Font = new System.Drawing.Font("Raleway", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UIStatut.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.UIStatut.Location = new System.Drawing.Point(354, 7);
+            this.UIStatut.Name = "UIStatut";
+            this.UIStatut.Size = new System.Drawing.Size(137, 37);
+            this.UIStatut.TabIndex = 21;
+            this.UIStatut.Text = "UIStatut";
+            this.UIStatut.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UITrackers
+            // 
+            this.UITrackers.Enabled = false;
+            this.UITrackers.Font = new System.Drawing.Font("Raleway Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UITrackers.FormattingEnabled = true;
+            this.UITrackers.ItemHeight = 25;
+            this.UITrackers.Location = new System.Drawing.Point(37, 86);
+            this.UITrackers.Name = "UITrackers";
+            this.UITrackers.Size = new System.Drawing.Size(800, 379);
+            this.UITrackers.TabIndex = 39;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Raleway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(32, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 29);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "Active Trackers";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Raleway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(32, 489);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 29);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "Add Trackers";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UITextboxSearch
+            // 
+            this.UITextboxSearch.Font = new System.Drawing.Font("Raleway Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UITextboxSearch.Location = new System.Drawing.Point(102, 525);
+            this.UITextboxSearch.Margin = new System.Windows.Forms.Padding(10);
+            this.UITextboxSearch.Name = "UITextboxSearch";
+            this.UITextboxSearch.Size = new System.Drawing.Size(437, 32);
+            this.UITextboxSearch.TabIndex = 43;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Raleway Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(38, 528);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 25);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "IP ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Raleway Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(102, 577);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 32);
+            this.textBox1.TabIndex = 45;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Raleway Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(38, 580);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 25);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Port";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UIUpload
+            // 
+            this.UIUpload.BackColor = System.Drawing.Color.LimeGreen;
+            this.UIUpload.FlatAppearance.BorderSize = 0;
+            this.UIUpload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UIUpload.Font = new System.Drawing.Font("Raleway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UIUpload.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.UIUpload.Location = new System.Drawing.Point(420, 577);
+            this.UIUpload.Name = "UIUpload";
+            this.UIUpload.Size = new System.Drawing.Size(119, 36);
+            this.UIUpload.TabIndex = 47;
+            this.UIUpload.Text = "Add";
+            this.UIUpload.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Raleway", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(884, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(222, 29);
+            this.label5.TabIndex = 49;
+            this.label5.Text = "Tracker information";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Enabled = false;
+            this.listBox1.Font = new System.Drawing.Font("Raleway Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(889, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(800, 379);
+            this.listBox1.TabIndex = 48;
+            // 
+            // TrackerControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.UIUpload);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.UITextboxSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UITrackers);
+            this.Controls.Add(this.UIStatut);
+            this.Controls.Add(this.label12);
+            this.Name = "TrackerControl";
+            this.Size = new System.Drawing.Size(1901, 897);
+            this.Load += new System.EventHandler(this.TrackerControl_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label UIStatut;
+        private System.Windows.Forms.ListBox UITrackers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UITextboxSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button UIUpload;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ Musics - Client/UI/TrackerControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utility.Network.Tracker.Identity;
+
+namespace Musics___Client.UI
+{
+    public partial class TrackerControl : UserControl
+    {
+        public List<TrackerIdentity> TrackersList = new List<TrackerIdentity>();
+
+        public TrackerControl()
+        {
+            InitializeComponent();
+        }
+
+        private void TrackerControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void AddTrackerToUI(TrackerIdentity ti)
+        {
+            TrackersList.Add(ti);
+            UpdateTrackerList();
+        }
+
+        public void UpdateTrackerList()
+        {
+            UITrackers.Items.Clear();
+            foreach(var ti in TrackersList)
+            {
+                UITrackers.Items.Add(ti.IPEndPoint.ToString());
+            }
+        }
+
+        public TrackerIdentity GetSelectedTracker()
+            => TrackersList[UITrackers.SelectedIndex];
+
+        public void UpdateStatut(ConnectionState connectionState)
+        {
+
+        }
+    }
+}
