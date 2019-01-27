985fd869f0ae169548a00f45d26d85ff6a3c93e7
Malaury <malaurydutour@outlook.fr>
2019-01-27 12:09:42 +0100

    Clien socket universal

--- Musics - Server/Network/ClientSocket.cs
+++ Musics - Server/Network/ClientSocket.cs
@@ -1,90 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using Utility.Network;
-using Utility.Network.Server;
+﻿using Utility.Network.Tracker;
 using Utility.Network.Tracker.Identity;
 
 namespace Musics___Server.Network
 {
-    public class ClientSocket
+    public class ClientSocket : ClientSetup
     {
-        public Socket _Socket { get; set; }
-
-        public delegate void PacketReceivedEvent(object sender, PacketEventArgs a);
-        public delegate void PacketReceivedHandler(object sender, PacketEventArgs a);
-        public event PacketReceivedHandler Packetreceived;
-
-        public Thread recevoir;
-
-        public void Connect(TrackerIdentity trackeridentity)
+        public async void Connect(TrackerIdentity trackeridentity)
         {
-            try
-            {
-                _Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                _Socket.BeginConnect(trackeridentity.IPEndPoint, new AsyncCallback(ConnectCallback), _Socket);
-            }
-            catch(Exception ex) {
-                Program.MyServer.Log.Critical(ex.Message);
-                throw ex;
-            }
+            SetupSocket(trackeridentity.IPEndPoint.Port, 1000);
+            await Connect(trackeridentity.IPEndPoint);
+            StartReceiving();
         }
-
-        private static int Bufferlgth = 10000;
-        byte[] recbuffer = new byte[Bufferlgth];
-
-        private void ConnectCallback(IAsyncResult ar)
-        {
-            try
-            {
-                _Socket = (Socket)ar.AsyncState;
-                _Socket.EndConnect(ar);
-                recevoir = new Thread(new ThreadStart(Receive));
-                recevoir.Start();
-                
-            }
-            catch (Exception ex)
-            {
-                Program.MyServer.Log.Critical(ex.Message);
-                throw ex;
-            }
-        }
-
-        public void Receive()
-        {
-            _Socket.BeginReceive(recbuffer, 0, Bufferlgth, SocketFlags.Partial,
-                    new AsyncCallback(ReceiveCallback), _Socket);
-        }
-
-        private void ReceiveCallback(IAsyncResult ar)
-        {
-            try
-            {
-                _Socket.EndReceive(ar);
-            }
-            catch
-            {
-
-            }
-
-            OnPacketReceived(new PacketEventArgs((IPacket)Function.Deserialize(new MessageTCP(recbuffer)),_Socket));
-
-            try
-            {
-                _Socket.BeginReceive(recbuffer, 0, recbuffer.Length, SocketFlags.Partial,
-                new AsyncCallback(ReceiveCallback), _Socket);
-            }
-            catch(Exception ex) {
-                Program.MyServer.Log.Critical(ex.Message);
-                throw ex;
-            }
-        }
-
-        public void OnPacketReceived(PacketEventArgs e)
-           => Packetreceived?.Invoke(null, e);
     }
 }
