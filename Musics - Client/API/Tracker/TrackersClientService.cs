e41ff3dfbc68f1348d62ccc1908cfc5971b9a867
Malaury <malaurydutour@outlook.fr>
2019-01-27 17:28:18 +0100

    Adding event to service for the UI

---
+++ Musics - Client/API/Tracker/TrackersClientService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Utility.Network;
+using Utility.Network.Tracker.Identity;
+using Utility.Network.Tracker.Requests;
+
+namespace Musics___Client.API.Tracker
+{
+    public class TrackersClientService
+    {
+        static private readonly Lazy<TrackersClientService> instance = new Lazy<TrackersClientService>(() => new TrackersClientService());
+        public static TrackersClientService Instance { get => instance.Value; }
+
+        private List<TrackerClientService> TrackersSocket = new List<TrackerClientService>();
+
+        public event EventHandler Registered;
+        public virtual void OnRegister(object sender, EventArgs e)
+            => Registered?.Invoke(sender, e);
+
+        public void Init()
+        {
+            var a = TrackerXml.GetServers();
+            foreach (var t in a)
+                AddTracker(t);
+        }
+
+        public void AddTracker(TrackerIdentity trackerIdentity)
+        {
+            TrackerClientService cs = new TrackerClientService();
+            cs.Packetreceived += Cs_Packetreceived;
+            cs.Connect(trackerIdentity);
+            while (!cs.IsConnected()) { }
+            var r = new Register()
+            {
+                Identity = new ClientIdentity()
+            };
+            cs.Send(Function.Serialize(r).Data);
+            TrackersSocket.Add(cs);
+        }
+
+        private void Cs_Packetreceived(object sender, Utility.Network.Server.PacketEventArgs a)
+        {
+            if (a.Packet is TrackerRequest)
+            {
+                if (a.Packet is RegisterAck)
+                {
+                    TrackerXml.AddServerToXml(new TrackerIdentity(a.Sender.RemoteEndPoint as IPEndPoint));
+                    OnRegister(a.Sender.RemoteEndPoint, new EventArgs());
+                }
+            }
+        }
+
+        public List<TrackerIdentity> RetreiveActiveTrackers()
+            => TrackersSocket.Select(t => t.Trackeridentity).ToList();
+    }
+}
---
+++ Utility/Network/TrackerXml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Linq;
+using System.Net;
+using Utility.Network.Tracker.Identity;
+
+namespace Utility.Network
+{
+    public static class TrackerXml
+    {
+        public const string Path = @"TrackerIP.xml";
+
+        public static void Setup()
+        {
+            if (!File.Exists(Path))
+            {
+                using (var writer = XmlWriter.Create(Path))
+                {
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("Trackers");
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                }
+            }
+        }
+
+        public static void AddServerToXml(TrackerIdentity si)
+        {
+            if (si == null) return;
+            if (IsExisting(si)) return;
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Path);
+
+            XmlNode nodeServer = doc.CreateElement("Tracker");
+
+            XmlNode nodeIP = doc.CreateElement("Ip");
+            nodeIP.InnerText = si.IPEndPoint.Address.ToString();
+            nodeServer.AppendChild(nodeIP);
+
+            XmlNode nodePort = doc.CreateElement("Port");
+            nodePort.InnerText = si.IPEndPoint.Port.ToString();
+            nodeServer.AppendChild(nodePort);
+
+            doc.DocumentElement.AppendChild(nodeServer);
+
+            doc.Save(Path);
+
+        }
+
+        public static bool IsExisting(TrackerIdentity identity)
+        {
+            foreach (var p in GetServers())
+            {
+                if (p.IPEndPoint.Address == identity.IPEndPoint.Address && p.IPEndPoint.Port == identity.IPEndPoint.Port)
+                    return true;
+            }
+            return false;
+        }
+
+        public static TrackerIdentity[] GetServers()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Path);
+            XmlNodeList nodes = doc.DocumentElement.SelectNodes("Tracker");
+            return nodes.Cast<XmlNode>().Select(x => GetServer(x)).ToArray();
+
+        }
+
+        public static TrackerIdentity GetServer(XmlNode node)
+        {
+            if (!IPAddress.TryParse(node["Ip"].InnerText, out IPAddress iP)) throw new Exception();
+            if (!int.TryParse(node["Port"].InnerText, out int i)) throw new Exception();
+            var si = new TrackerIdentity(new IPEndPoint(iP, i));
+
+            return si;
+        }
+    }
+}
