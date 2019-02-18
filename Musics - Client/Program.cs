03684dc67853826f2601ecc8cdb5e8573a22eb9d
Malaury <malaurydutour@outlook.fr>
2019-02-18 19:01:12 +0100

    Fixe close issue

--- Musics - Client/Program.cs
+++ Musics - Client/Program.cs
@@ -22,7 +22,7 @@ namespace Musics___Client
             Loginf = new LoginPageForm();
             Loginf.login1.LoginCompleted += Login1_LoginCompleted;
             Application.Run(Loginf);
-
+            
             // Application.Run(new Client());
             //Application.Run(new LoginPage());
             //
@@ -32,7 +32,14 @@ namespace Musics___Client
         {
             Loginf.Hide();
             Cl = new Client(e.CryptedCredentials);
+            Cl.FormClosed += Cl_FormClosed;
             Cl.ShowDialog();
+            
+        }
+
+        private static void Cl_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(Environment.ExitCode);
         }
     }
 }
