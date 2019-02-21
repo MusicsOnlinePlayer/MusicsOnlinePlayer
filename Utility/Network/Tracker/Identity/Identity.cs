8813c31d27d681515d09da3f27e36ef9896b8717
Malaury <malaurydutour@outlook.fr>
2019-02-21 11:09:21 +0100

    Promote user for P2P

--- Utility/Network/Tracker/Identity/Identity.cs
+++ Utility/Network/Tracker/Identity/Identity.cs
@@ -7,5 +7,23 @@ namespace Utility.Network.Tracker.Identity
     public abstract class Identity : IIdentity
     {
         public virtual IPEndPoint IPEndPoint { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Identity)) return false;
+
+            var item = obj as Identity;
+
+            return item.IPEndPoint.ToString() == this.IPEndPoint.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return IPEndPoint.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return IPEndPoint.ToString();
+        }
     }
 }
